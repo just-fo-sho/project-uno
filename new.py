5b62b16360c78694d2e1721c471da790419b9842
just-fo-sho <shokawano@yahoo.com>
2019-09-09 16:38:31 -0700

    Comitting new file from Python for Data Science Book.  Gotta keep practicing

---
+++ new.py
@@ -0,0 +1,10 @@
+#-------------------------------------
+# file: myscript.py
+# Used for Python for Data Science Learning
+
+def square(x):
+    """square a number"""
+    return x ** 2
+
+for N in range(1, 4):
+    print(N, "squared is", square(N))
